9780523793ec96d7d6ca24026c7c6bcca00ec971
Arman <armanphaugat20@gmail.com>
2026-04-01 11:15:05 +0530

    ReadMe Updated

--- exercises/squat.py
+++ exercises/squat.py
@@ -5,23 +5,16 @@ class Squat:
         self.counter = 0
 
     def analyze(self, landmarks):
-
         hip = [landmarks[23].x, landmarks[23].y]
         knee = [landmarks[25].x, landmarks[25].y]
         ankle = [landmarks[27].x, landmarks[27].y]
-
         angle = calculate_angle(hip, knee, ankle)
-
         feedback = ""
-
         if angle > 160:
             self.stage = "up"
-
         if angle < 90 and self.stage == "up":
             self.stage = "down"
             self.counter += 1
-
         if angle > 120 and angle < 160:
             feedback = "Go Lower"
-
         return angle, feedback, self.counter
